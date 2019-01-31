80c48263f129398d24a0b67aca6f92b873e691a2
umbur <work1415@gmail.com>
2019-01-31 16:05:12 -0700

    POST request works

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -5,27 +5,46 @@ import Friend from './Friend';
 import { fetchFriends } from '../actions';
 
 class FriendsList extends React.Component {
-    componentDidMount() {
-        this.props.fetchFriends();
-    }
+
+  componentDidMount() {
+
+    this.props.fetchFriends();
+
+  }
 
   render() {
-    const { friends, fetching } = this.props;
-        if (fetching)
-            return <h1>Fetching friends...</h1>;
-        return (
-            <div className='friends-list'>
-                {friends.map((friend, id) => <Friend key={id} friend={friend} />)}
-            </div>
-        );
-    }
+
+    const { friends, fetching, error } = this.props;
+
+    console.log(error);
+
+    if (fetching)
+      return <h1>Fetching friends...</h1>;
+
+    return (
+
+      <div className='friends-list'>
+
+        {friends.map((friend, id) => <Friend key={id} friend={friend} />)}
+
+      </div>
+
+    );
+
+  }
+
 }
 
 function mapStateToProps(state) {
-    return {
-        friends: state.friends,
-        fetching: state.fetching
-    }
+
+  return {
+
+    friends: state.friends,
+    fetching: state.fetching,
+    error: state.error
+
+  }
+
 }
 
 export default connect(mapStateToProps, { fetchFriends })(FriendsList);
